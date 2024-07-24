a09dc9d50f8fef9bc0218d61e756ce3fcb3febd2
DKTDeanThomas <deanthomas634@gmail.com>
2024-07-24 14:54:56 +0200

    Cursor movement

---
+++ Assignment One Clone/Assets/Scripts/CursorObject.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorObject : MonoBehaviour
+{
+
+    [SerializeField] private CursorManager.CursorType cursorType;
+
+    private bool isMouseOver = false;
+
+    private void OnMouseOver()
+    {
+        if (!isMouseOver)
+        {
+            CursorManager.Instance.SetActiveCursorType(cursorType);
+            isMouseOver = true;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Mouse2))
+        {
+            Debug.Log("Lock tf in");
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        CursorManager.Instance.SetActiveCursorType(CursorManager.CursorType.Arrow);
+        isMouseOver = false;
+    }
+}
--- Assignment One Clone/Assets/Scripts/cameraFollow.cs
+++ Assignment One Clone/Assets/Scripts/cameraFollow.cs
@@ -2,15 +2,44 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class cameraFollow : MonoBehaviour
+public class CameraFollow : MonoBehaviour
 {
     public Transform player;
     public Vector3 offset;
+    public float smoothSpeed = 0.125f;
+    public float edgeOffset = 100f;
 
+    private Vector3 velocity = Vector3.zero;
 
-    // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        transform.position = new Vector3(player.position.x + offset.x, player.position.y + offset.y, offset.z);
+        Vector3 desiredPosition = player.position + offset;
+        Vector3 mousePos = Input.mousePosition;
+
+        // Calculate screen edges
+        float screenWidth = Screen.width;
+        float screenHeight = Screen.height;
+
+        // Adjust camera position based on cursor position
+        if (mousePos.x < edgeOffset)
+        {
+            desiredPosition.x -= (edgeOffset - mousePos.x) / edgeOffset;
+        }
+        if (mousePos.x > screenWidth - edgeOffset)
+        {
+            desiredPosition.x += (mousePos.x - (screenWidth - edgeOffset)) / edgeOffset;
+        }
+        if (mousePos.y < edgeOffset)
+        {
+            desiredPosition.y -= (edgeOffset - mousePos.y) / edgeOffset;
+        }
+        if (mousePos.y > screenHeight - edgeOffset)
+        {
+            desiredPosition.y += (mousePos.y - (screenHeight - edgeOffset)) / edgeOffset;
+        }
+
+        // Smooth camera movement
+        Vector3 smoothedPosition = Vector3.SmoothDamp(transform.position, desiredPosition, ref velocity, smoothSpeed);
+        transform.position = smoothedPosition;
     }
 }
