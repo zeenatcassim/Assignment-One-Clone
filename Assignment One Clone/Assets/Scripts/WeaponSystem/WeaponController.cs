600ebfe7da84dba0411eb8fb5ee4cb776c713e1e
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-07-30 01:16:07 +0200

    Weapon System Update

--- Assignment One Clone/Assets/Scripts/WeaponSystem/WeaponController.cs
+++ Assignment One Clone/Assets/Scripts/WeaponSystem/WeaponController.cs
@@ -4,15 +4,45 @@ using UnityEngine;
 
 public class WeaponController : MonoBehaviour
 {
+    [Header("Player-Gun Spawn Transform")]
+    [SerializeField] Transform playerTransform;
+
+    [Header("Weapon Switching")]
+    [SerializeField] bool canCollect = false;
+    [SerializeField] int weaponToggle = 0;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            canCollect = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            canCollect = false;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            Debug.Log("Collect");
+        }
+
+        this.transform.position = playerTransform.position;
     }
 }
