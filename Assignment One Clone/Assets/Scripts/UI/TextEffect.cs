b244d4409571c2541a61fdf92281a37070b7d30d
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-07-31 21:32:39 +0200

    UI Update

---
+++ Assignment One Clone/Assets/Scripts/UI/TextEffect.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class TextEffect : MonoBehaviour
+{
+    [SerializeField] TMP_Text text;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.ForceMeshUpdate();
+        var textArr = text.textInfo;
+
+        for (int i = 0; i < textArr.characterCount; i++)
+        {
+            var charVar = textArr.characterInfo[i];
+
+            if (!charVar.isVisible) continue;
+
+            var charVertices = textArr.meshInfo[charVar.materialReferenceIndex].vertices;
+
+            for (int j = 0; j < 4; j++)
+            {
+                var orig = charVertices[charVar.vertexIndex + j];
+                charVertices[charVar.vertexIndex + j] = orig + new Vector3(0, Mathf.Sin(Time.time * 2.5f + orig.x * (0.01f)) * 5f , 0);
+            }
+        }
+
+        for (int i = 0;i < textArr.meshInfo.Length; i++)
+        {
+            var meshInfo = textArr.meshInfo[i];
+            meshInfo.mesh.vertices = meshInfo.vertices;
+            text.UpdateGeometry(meshInfo.mesh, i);
+        }
+    }
+}
