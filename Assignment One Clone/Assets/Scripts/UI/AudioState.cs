7df858787659bf122d017ef28da1311e2820514a
omphilejmatsobe <omphilejmatsobe@gmail.com>
2024-07-31 15:39:56 +0200

    Weapon System Update

---
+++ Assignment One Clone/Assets/Scripts/UI/AudioState.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "Audio", menuName = "ScriptableObjects/Audio", order = 2)]
+public class AudioState : ScriptableObject
+{
+    public bool state;
+}
