d149b0181c76f488978290fb1e83613995a3465a
zeenatcassim <116756046+zeenatcassim@users.noreply.github.com>
2024-08-02 14:19:25 +0200

    throw gun updated

--- Assignment One Clone/Assets/Scripts/ranged combat/gunAmmo.cs
+++ Assignment One Clone/Assets/Scripts/ranged combat/gunAmmo.cs
@@ -5,12 +5,12 @@ using UnityEngine;
 public class gunAmmo : MonoBehaviour
 {
     public int maxAmmo = 17;
-    private int currentAmmo;
+  //  private int currentAmmo;
 
     // Start is called before the first frame update
     void Start()
     {
-        currentAmmo = maxAmmo;
+        //currentAmmo = maxAmmo;
     }
 
     // Update is called once per frame
